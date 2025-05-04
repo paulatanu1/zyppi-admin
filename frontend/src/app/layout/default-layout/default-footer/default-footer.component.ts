e73e7bb2b6ba47e30b4fbc9c17e7ca6b00ac04f5
paulatanu1 <atanupaul76@gmail.com>
2025-05-04 20:44:56 +0530

    route set

--- frontend/src/app/layout/default-layout/default-footer/default-footer.component.ts
+++ frontend/src/app/layout/default-layout/default-footer/default-footer.component.ts
@@ -2,12 +2,16 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '@coreui/angular';
 
 @Component({
-    selector: 'app-default-footer',
-    templateUrl: './default-footer.component.html',
-    styleUrls: ['./default-footer.component.scss']
+  selector: 'app-default-footer',
+  templateUrl: './default-footer.component.html',
+  styleUrls: ['./default-footer.component.scss'],
 })
 export class DefaultFooterComponent extends FooterComponent {
+  currentYear: string;
+
   constructor() {
     super();
+    this.currentYear = new Date().getFullYear().toString();
+    console.log(this.currentYear);
   }
 }
